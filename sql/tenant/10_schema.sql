2633cf12273b55a335e4af157ef72cc26e33a344
allister0098 <s1210098@gmail.com>
2022-07-23 13:57:09 +0900

    [modify]sqliteの文法に直す

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE competition (
   title TEXT NOT NULL,
   finished_at BIGINT NULL,
   created_at BIGINT NOT NULL,
-  created_at_desc BIGINT AS (-created_at) STORED NOT NULL,
+  created_at_desc BIGINT GENERATED ALWAYS AS (-created_at) STORED NOT NULL,
   updated_at BIGINT NOT NULL,
   INDEX `tenant_id_created_at` (`tenant_id`, `created_at`),
   INDEX `tenant_id_created_at_desc` (`tenant_id`, `created_at_desc`)
